0801bb93ba7bcf2f4136ae1316fde9be808122c8
trupti-gitaccount <truptimayeebhuyan02@gmail.com>
2024-03-07 18:24:40 +0530

    127: create save post Feature Entity  class added

---
+++ src/main/java/com/myblog/myblog11/entity/post.java
@@ -0,0 +1,22 @@
+package com.myblog.myblog11.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="posts")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class post {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+
+    private long id;
+
+    private String title;
+    private String description;
+    private String content;
+}
